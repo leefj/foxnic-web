3d64b7e91931df7fb79fe7772897590442b19ca1
lifangjie <leefangjie@qq.com>
2021-06-28 17:44:36 +0800

    临时的提交

--- common/generator/src/main/java/org/github/foxnic/web/generator/data/MenuGenerator.java
+++ common/generator/src/main/java/org/github/foxnic/web/generator/data/MenuGenerator.java
@@ -13,6 +13,7 @@ import org.github.foxnic.web.constants.db.FoxnicWeb.SYS_MENU;
 import org.github.foxnic.web.constants.db.FoxnicWeb.SYS_ROLE_MENU;
 import org.github.foxnic.web.constants.enums.MenuType;
 import org.github.foxnic.web.domain.oauth.Menu;
+import org.github.foxnic.web.domain.oauth.Resourze;
 import org.github.foxnic.web.generator.config.FoxnicWebConfigs;
 import org.github.foxnic.web.oauth.page.ResourzePageController;
 import org.github.foxnic.web.proxy.oauth.ResourzeServiceProxy;
@@ -24,8 +25,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
- 
 
 public class MenuGenerator {
 	
@@ -60,7 +62,7 @@ public class MenuGenerator {
 	
 	
 	
-	
+	private static String CATALOG="新建:insert;删除:delete;批量删除:delete-by-ids;保存:update,save;查询:get-by-id,get-by-ids,query-list,query-paged-list;导出:export-excel;导入:export-excel-template,import-excel";
 	
 	
 	
@@ -73,176 +75,232 @@ public class MenuGenerator {
 		this.dao=this.configs.getDAO();
 	}
 	
-	public void removeByBatchId(String batchId) throws Exception {
 
-	 this.dao.beginTransaction();
-	 this.dao.execute("delete from sys_role_menu where menu_id in (select id from sys_menu where batch_id=?)",batchId);
-	 this.dao.execute("delete from sys_menu where batch_id=?",batchId);
-	 this.dao.commit();
-	 
-
-	}
 	
 	public void generate(DBTable table, Class proxy,Class page,String parentId) throws Exception {
 
-		String batchId=generateMenus(table,proxy,page,parentId);
-		
-		grant(batchId);
-		
-		System.err.println("batchId = "+batchId);
-
-	}
-
-	private void grant(String batchId) {
-		
-
-		RcdSet menus=dao.query("select id from "+SYS_MENU.$NAME+" where batch_id=?",batchId);
-		for (Rcd r : menus) {
-			dao.insert(SYS_ROLE_MENU.$NAME)
-			.set(SYS_ROLE_MENU.ID, IDGenerator.getSnowflakeId())
-			.set(SYS_ROLE_MENU.ROLE_ID, superAdminRoleId)
-			.set(SYS_ROLE_MENU.MENU_ID, r.getString(SYS_MENU.ID))
-			.set(SYS_ROLE_MENU.CREATE_BY,"auto")
-			.execute();
-		}
-	}
- 
-	
-	
-	private String generateMenus(DBTable table, Class proxy, Class page, String parentId) throws Exception {
-		
-		String authorityPrefix=table.name()+"::";
-		//批次号
 		String batchId=IDGenerator.getSnowflakeIdString();
-		
-		DBTableMeta tm=dao.getTableMeta(table.name());
-		
-		String topic=tm.getTopic();
-		topic=StringUtil.removeLast(topic, "数据表");
-		topic=StringUtil.removeLast(topic, "表");
-	 
-		 
-		//插入模块目录
-		Menu listMenu=null;
-		Menu formMenu=null;
-		
-		
-		
+
+		Menu indexMenu=createFromPage(page,table,batchId);
+
+
+
+	}
+
+	private Menu createFromPage(Class page,DBTable table,String batchId) throws Exception {
 		String prefix=(String)page.getDeclaredField("prefix").get(null);
 		prefix=StringUtil.removeFirst(prefix, "/");
+		List<Menu> pages=new ArrayList<>();
 		Method[] ms=page.getDeclaredMethods();
+		Menu indexMenu=null;
+		Menu formMenu=null;
+
 		for (Method m : ms) {
 			RequestMapping rm=m.getAnnotation(RequestMapping.class);
 			if(rm==null) continue;
 			String path="/"+StringUtil.joinUrl(prefix,rm.value()[0]);
 			System.out.println(path);
-			
- 
-			
+
+			Resourze resourze=new Resourze();
+			resourze.setId(IDGenerator.getSnowflakeIdString());
+			resourze.setUrl(path);
+			resourze.setType("page");
+			resourze.setMethod("GET");
+
 			Menu menu=new Menu();
 			menu.setId(IDGenerator.getSnowflakeIdString());
 			menu.setPath(path);
 			menu.setType(MenuType.page.name());
-			
 			if(path.endsWith("_form.html")) {
 				menu.setUrl("#!"+table.name().toLowerCase()+"_edit");
-				
-				menu.setLabel("编辑"+topic);
+				menu.setLabel("编辑"+getTopic(table));
 				menu.setHidden(1);
-			
 				formMenu=menu;
 			}
-			
 			if(path.endsWith("_list.html")) {
 				menu.setUrl("#!"+table.name().toLowerCase()+"_mngr");
-				menu.setLabel(topic+"管理");
+				menu.setLabel(getTopic(table)+"管理");
 				menu.setHidden(0);
-				listMenu=menu;
+				indexMenu=menu;
 			}
-			
 			menu.setSort(0);
-			
+			pages.add(menu);
 		}
-		
-		if(formMenu==null || listMenu==null) return null;
-		
-		listMenu.setAuthority(authorityPrefix+"list::view");
-		listMenu.setBatchId(batchId);
-		formMenu.setBatchId(batchId);
-		listMenu.setParentId(parentId);
-		formMenu.setParentId(listMenu.getId());
-		formMenu.setAuthority(authorityPrefix+"form::view");
-		dao.insertEntity(formMenu);
-		dao.insertEntity(listMenu);	
-	 
-		
-		
-		Menu api=new Menu();
-		api.setId(IDGenerator.getSnowflakeIdString());
-		api.setLabel("api");
-		api.setHidden(1);
-		api.setType(MenuType.folder.name());
-		api.setParentId(listMenu.getId());
-		api.setAuthority(authorityPrefix+"apiset");
-		api.setBatchId(batchId);
-		api.setSort(3);
-		dao.insertEntity(api);	
-		
-		File file=this.configs.getProxyProject().getSourceFile(UserServiceProxy.class);
-		String source=FileUtil.readText(file);
-		String[] lines=source.split("\n");
-		
-		
-		Field[] fs=proxy.getDeclaredFields();
 
-		int sort=0;
-		for (Field f : fs) {
-			String name=f.getName();
-			//排除一些无效的静态属性
-			if(name.equals("CONTROLLER_CLASS_NAME") || name.startsWith("API_") ) continue;
-			String value=(String)f.get(null);
-			String doc=this.findDoc(f.getName(),source,lines);
-			System.out.println(name + " = "+value+" , "+doc);
-			
-			Menu rest=new Menu();
-			rest.setId(IDGenerator.getSnowflakeIdString());
-			rest.setType(MenuType.api.name());
-			rest.setHidden(1);
-			rest.setAuthority(authorityPrefix+"api::"+name.toLowerCase());
-			rest.setParentId(api.getId());
-			rest.setPath(value);
-			rest.setLabel(doc);
-			rest.setBatchId(batchId);
-			rest.setSort(sort);
-			dao.insertEntity(rest);	
-			sort++;
-			
-		}
-		
-		return batchId;
+
+
+
+		return  indexMenu;
 	}
 
-	private String findDoc(String name, String source, String[] lines) {
-		String line=null;
-		int index=-1;
-		for (int i = 0; i < lines.length; i++) {
-			line=lines[i];
-			if(line.contains(" "+name+" ") || line.contains(" "+name+"=")) {
-				index=i;
-				break;
-			}
-		}
-		if(index==-1) return name;
-		
-		String doc=lines[index-2];
-		doc=doc.trim();
-		doc=StringUtil.removeFirst(doc,"*").trim();
-		return doc;
+
+	public String getTopic(DBTable table) {
+		DBTableMeta tm=dao.getTableMeta(table.name());
+		String topic=tm.getTopic();
+		topic=StringUtil.removeLast(topic, "数据表");
+		topic=StringUtil.removeLast(topic, "表");
+		return  topic;
 	}
 
-	
-	
-	
+
+
+//	private void grant(String batchId) {
+//
+//
+//		RcdSet menus=dao.query("select id from "+SYS_MENU.$NAME+" where batch_id=?",batchId);
+//		for (Rcd r : menus) {
+//			dao.insert(SYS_ROLE_MENU.$NAME)
+//			.set(SYS_ROLE_MENU.ID, IDGenerator.getSnowflakeId())
+//			.set(SYS_ROLE_MENU.ROLE_ID, superAdminRoleId)
+//			.set(SYS_ROLE_MENU.MENU_ID, r.getString(SYS_MENU.ID))
+//			.set(SYS_ROLE_MENU.CREATE_BY,"auto")
+//			.execute();
+//		}
+//	}
+//
+//
+//
+//	private String generateMenus(DBTable table, Class proxy, Class page, String parentId) throws Exception {
+//
+//		String authorityPrefix=table.name()+"::";
+//		//批次号
+//		String batchId=IDGenerator.getSnowflakeIdString();
+//
+//		DBTableMeta tm=dao.getTableMeta(table.name());
+//
+//		String topic=tm.getTopic();
+//		topic=StringUtil.removeLast(topic, "数据表");
+//		topic=StringUtil.removeLast(topic, "表");
+//
+//
+//		//插入模块目录
+//		Menu indexMenu=null;
+//		Menu formMenu=null;
+//
+//		String prefix=(String)page.getDeclaredField("prefix").get(null);
+//		prefix=StringUtil.removeFirst(prefix, "/");
+//		Method[] ms=page.getDeclaredMethods();
+//		for (Method m : ms) {
+//			RequestMapping rm=m.getAnnotation(RequestMapping.class);
+//			if(rm==null) continue;
+//			String path="/"+StringUtil.joinUrl(prefix,rm.value()[0]);
+//			System.out.println(path);
+//
+//
+//
+//			Menu menu=new Menu();
+//			menu.setId(IDGenerator.getSnowflakeIdString());
+//			menu.setPath(path);
+//			menu.setType(MenuType.page.name());
+//
+//			if(path.endsWith("_form.html")) {
+//				menu.setUrl("#!"+table.name().toLowerCase()+"_edit");
+//
+//				menu.setLabel("编辑"+topic);
+//				menu.setHidden(1);
+//
+//				formMenu=menu;
+//			}
+//
+//			if(path.endsWith("_list.html")) {
+//				menu.setUrl("#!"+table.name().toLowerCase()+"_mngr");
+//				menu.setLabel(topic+"管理");
+//				menu.setHidden(0);
+//				indexMenu=menu;
+//			}
+//
+//			menu.setSort(0);
+//
+//		}
+//
+//		if(formMenu==null || indexMenu==null) return null;
+//
+//		indexMenu.setAuthority(authorityPrefix+"list::view");
+//		indexMenu.setBatchId(batchId);
+//		formMenu.setBatchId(batchId);
+//		indexMenu.setParentId(parentId);
+//		formMenu.setParentId(indexMenu.getId());
+//		formMenu.setAuthority(authorityPrefix+"form::view");
+//		dao.insertEntity(formMenu);
+//		dao.insertEntity(indexMenu);
+//
+//
+//
+//		Menu api=new Menu();
+//		api.setId(IDGenerator.getSnowflakeIdString());
+//		api.setLabel("api");
+//		api.setHidden(1);
+//		api.setType(MenuType.folder.name());
+//		api.setParentId(indexMenu.getId());
+//		api.setAuthority(authorityPrefix+"apiset");
+//		api.setBatchId(batchId);
+//		api.setSort(3);
+//		dao.insertEntity(api);
+//
+//		File file=this.configs.getProxyProject().getSourceFile(UserServiceProxy.class);
+//		String source=FileUtil.readText(file);
+//		String[] lines=source.split("\n");
+//
+//
+//		Field[] fs=proxy.getDeclaredFields();
+//
+//		int sort=0;
+//		for (Field f : fs) {
+//			String name=f.getName();
+//			//排除一些无效的静态属性
+//			if(name.equals("CONTROLLER_CLASS_NAME") || name.startsWith("API_") ) continue;
+//			String value=(String)f.get(null);
+//			String doc=this.findDoc(f.getName(),source,lines);
+//			System.out.println(name + " = "+value+" , "+doc);
+//
+//			Menu rest=new Menu();
+//			rest.setId(IDGenerator.getSnowflakeIdString());
+//			rest.setType(MenuType.api.name());
+//			rest.setHidden(1);
+//			rest.setAuthority(authorityPrefix+"api::"+name.toLowerCase());
+//			rest.setParentId(api.getId());
+//			rest.setPath(value);
+//			rest.setLabel(doc);
+//			rest.setBatchId(batchId);
+//			rest.setSort(sort);
+//			dao.insertEntity(rest);
+//			sort++;
+//
+//		}
+//
+//		return batchId;
+//	}
+//
+//	private String findDoc(String name, String source, String[] lines) {
+//		String line=null;
+//		int index=-1;
+//		for (int i = 0; i < lines.length; i++) {
+//			line=lines[i];
+//			if(line.contains(" "+name+" ") || line.contains(" "+name+"=")) {
+//				index=i;
+//				break;
+//			}
+//		}
+//		if(index==-1) return name;
+//
+//		String doc=lines[index-2];
+//		doc=doc.trim();
+//		doc=StringUtil.removeFirst(doc,"*").trim();
+//		return doc;
+//	}
+//
+//
+
+
+
+	public void removeByBatchId(String batchId) throws Exception {
+
+		this.dao.beginTransaction();
+		this.dao.execute("delete from sys_role_menu where menu_id in (select id from sys_menu where batch_id=?)",batchId);
+		this.dao.execute("delete from sys_menu where batch_id=?",batchId);
+		this.dao.commit();
+
+	}
 	
 	
 
--- common/generator/src/main/java/org/github/foxnic/web/generator/module/OAuthCodeGenerator.java
+++ common/generator/src/main/java/org/github/foxnic/web/generator/module/OAuthCodeGenerator.java
@@ -16,6 +16,7 @@ public class OAuthCodeGenerator extends SystemCodeGenerator {
 	public static void main(String[] args) throws Exception {
 		OAuthCodeGenerator g=new OAuthCodeGenerator();
 		g.generateSysResource();
+		g.generateSysMenuResource();
 //		g.generateSysUser();
 //		g.generateSysOAuthClient();
 //		g.generateSysSessionOnline();
@@ -134,11 +135,11 @@ public class OAuthCodeGenerator extends SystemCodeGenerator {
 
 		//文件生成覆盖模式
 		cfg.overrides()
-				.setServiceIntfAnfImpl(WriteMode.COVER_EXISTS_FILE) //服务与接口
-				.setControllerAndAgent(WriteMode.COVER_EXISTS_FILE) //Rest
-				.setPageController(WriteMode.COVER_EXISTS_FILE) //页面控制器
-				.setFormPage(WriteMode.COVER_EXISTS_FILE) //表单HTML页
-				.setListPage(WriteMode.COVER_EXISTS_FILE); //列表HTML页
+				.setServiceIntfAnfImpl(WriteMode.CREATE_IF_NOT_EXISTS) //服务与接口
+				.setControllerAndAgent(WriteMode.CREATE_IF_NOT_EXISTS) //Rest
+				.setPageController(WriteMode.CREATE_IF_NOT_EXISTS) //页面控制器
+				.setFormPage(WriteMode.CREATE_IF_NOT_EXISTS) //表单HTML页
+				.setListPage(WriteMode.CREATE_IF_NOT_EXISTS); //列表HTML页
 
 		//生成代码
 		cfg.buildAll();
