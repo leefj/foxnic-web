c448de59898d81f453e05d7c586fa37fcb210166
lifangjie <leefangjie@qq.com>
2021-07-05 16:02:14 +0800

    调整SQL扫描范围

--- common/framework-boot/src/main/java/org/github/foxnic/web/framework/dao/DBConfigs.java
+++ common/framework-boot/src/main/java/org/github/foxnic/web/framework/dao/DBConfigs.java
@@ -1,18 +1,5 @@
 package org.github.foxnic.web.framework.dao;
 
-import javax.sql.DataSource;
-
-import org.github.foxnic.web.constants.db.FoxnicWeb.SYS_SEQUENCE;
-import org.github.foxnic.web.relation.FoxnicWebRelationManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.github.foxnic.commons.log.Logger;
@@ -21,6 +8,19 @@ import com.github.foxnic.dao.spec.DAOBuilder;
 import com.github.foxnic.dao.sql.loader.SQLoader;
 import com.github.foxnic.springboot.spring.SpringUtil;
 import com.github.foxnic.sql.treaty.DBTreaty;
+import org.github.foxnic.web.constants.db.FoxnicWeb.SYS_SEQUENCE;
+import org.github.foxnic.web.relation.FoxnicWebRelationManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DBConfigs {
@@ -70,11 +70,18 @@ public class DBConfigs {
 			//设置序列相关的配置
 			dao.setSequenceTable(SYS_SEQUENCE.$NAME);
 			dao.setSequenceProcedure("NEXT_VAL");
-			
-			
+
+
 			//设置SQL扫描
-			SQLoader.setTQLScanPackage(dao,SpringUtil.getStartupClass().getPackage().getName());
-			
+			SQLoader.addTQLScanPackage(dao,SpringUtil.getStartupClass().getPackage().getName());
+			ComponentScan sc=(ComponentScan)SpringUtil.getStartupClass().getAnnotation(ComponentScan.class);
+			for (String pkg : sc.basePackages()) {
+				SQLoader.addTQLScanPackage(dao,pkg);
+			}
+
+
+
+
 			return dao;
 		} catch (Exception e) {
 			Logger.error("创建DAO失败",e);
