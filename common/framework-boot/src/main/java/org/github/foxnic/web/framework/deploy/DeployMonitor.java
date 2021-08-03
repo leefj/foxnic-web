f760ece61476732c248af261773dd8e8a41f97f4
lifangjie <leefangjie@qq.com>
2021-08-03 09:57:34 +0800

    静态资源快速部署实现

--- common/framework-boot/src/main/java/org/github/foxnic/web/framework/deploy/DeployMonitor.java
+++ common/framework-boot/src/main/java/org/github/foxnic/web/framework/deploy/DeployMonitor.java
@@ -2,10 +2,10 @@ package org.github.foxnic.web.framework.deploy;
 
 import com.github.foxnic.commons.concurrent.task.SimpleTaskManager;
 import com.github.foxnic.commons.io.FileUtil;
+import com.github.foxnic.commons.lang.DateUtil;
 import com.github.foxnic.commons.log.Logger;
 import com.github.foxnic.commons.project.maven.MavenProject;
 import com.github.foxnic.springboot.spring.SpringUtil;
-import com.sun.nio.file.SensitivityWatchEventModifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
@@ -26,9 +26,14 @@ public class DeployMonitor implements ApplicationListener<ApplicationStartedEven
     @Value("${develop.deploy.projects}")
     private String projects;
 
+    @Value("${develop.deploy.enable}")
+    private Boolean enable=false;
+
+
     private List<MavenProject> mps=null;
 
     private void regist() throws Exception {
+
         watcher = FileSystems.getDefault().newWatchService();
 
         mps=new ArrayList<>();
@@ -40,26 +45,42 @@ public class DeployMonitor implements ApplicationListener<ApplicationStartedEven
         String[] dirs=projects.split(",");
         for (String dir : dirs) {
             File d=FileUtil.resolveByPath(wrapper.getProjectDir().getParentFile(),dir.trim());
+            d=new File(d.getCanonicalPath());
             MavenProject mp=new MavenProject(d);
             mps.add(mp);
         }
         //注册监控目录
-        File dir;
-        Path mPath;
+        File dir=null;
         for (MavenProject mp : mps) {
             dir=FileUtil.resolveByPath(mp.getMainResourceDir(),"/public");
             if(dir.exists()) {
-                mPath = Paths.get(dir.getAbsolutePath());
-                mPath.register(watcher, new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_MODIFY}, SensitivityWatchEventModifier.HIGH);
+                register(dir);
             }
             dir=FileUtil.resolveByPath(mp.getMainResourceDir(),"/static");
             if(dir.exists()) {
-                mPath = Paths.get(dir.getAbsolutePath());
-                mPath.register(watcher, new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_MODIFY}, SensitivityWatchEventModifier.HIGH);
+                register(dir);
             }
         }
+
+        Logger.info("deploy is ready on "+monitorDirCount+" dirs");
+
         //启动监控
         watching();
+    }
+
+    private int monitorDirCount=0;
+
+    private void register(File dir) throws Exception {
+        monitorDirCount++;
+        //System.out.println("register\t"+dir.getAbsolutePath());
+        Path mPath= Paths.get(dir.getAbsolutePath());
+        mPath.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,StandardWatchEventKinds.ENTRY_MODIFY,StandardWatchEventKinds.ENTRY_DELETE,StandardWatchEventKinds.OVERFLOW);
+        File[] fs=dir.listFiles();
+        for (File f : fs) {
+           if(f.isDirectory()) {
+               register(f);
+           }
+        }
     }
 
     private int taskId=-1;
@@ -77,14 +98,17 @@ public class DeployMonitor implements ApplicationListener<ApplicationStartedEven
                 WatchEvent.Kind<?> kind = event.kind();
                 final Path path = (Path) key.watchable();
                 final Path filename = (Path) event.context();
-                if(filename.toString().endsWith("~")) continue;
-                final String fullPath=String.format("%s\\%s%n", path.toString(), filename.toString());
+                final String fullPath=String.format("%s\\%s%n", path.toString(), filename.toString()).trim();
+                File target=new File(fullPath);
+                if(target.isDirectory()) continue;
+                if(filename.toString().trim().endsWith("~")) continue;
+//                System.err.println(fullPath);
                 tm.clearTask(taskId);
                 taskId=tm.doDelayTask(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            refresh(new File(fullPath));
+                            deploy(target);
                         } catch (Exception e) {
                             Logger.error("watching method eror",e);
                         }
@@ -101,7 +125,8 @@ public class DeployMonitor implements ApplicationListener<ApplicationStartedEven
         }
     }
 
-    private void refresh(File file) {
+    private void deploy(File file) {
+        if(file.isDirectory()) return;
         MavenProject p=null;
         for (MavenProject mp : mps) {
             if(file.getAbsolutePath().startsWith(mp.getProjectDir().getAbsolutePath())) {
@@ -112,15 +137,16 @@ public class DeployMonitor implements ApplicationListener<ApplicationStartedEven
         if(p==null) {
             throw new RuntimeException(file.getAbsolutePath()+" 所属项目未注册");
         }
+
         String suffix=file.getAbsolutePath().substring(p.getMainResourceDir().getAbsolutePath().length());
         File to=FileUtil.resolveByPath(p.getTargetClassesDir(),suffix);
         FileUtil.copyFile(file,to);
-
-        System.out.println(System.currentTimeMillis()+" "+file.getAbsolutePath());
+        System.err.println("["+ DateUtil.getCurrTime("HH:mm:ss") +"] deploy "+" "+p.getProjectDir().getName()+" "+suffix);
     }
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
+        if(!enable) return;
         SimpleTaskManager.doParallelTask(new Runnable() {
             @Override
             public void run() {
