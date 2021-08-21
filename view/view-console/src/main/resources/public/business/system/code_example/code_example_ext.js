c0302419d923a05223dfd675521175ed6f4c87f1
LeeFJ <leefangjie@qq.com>
2021-08-21 20:38:26 +0800

    版本升级

--- view/view-console/src/main/resources/public/business/system/code_example/code_example_ext.js
+++ view/view-console/src/main/resources/public/business/system/code_example/code_example_ext.js
@@ -6,9 +6,9 @@ layui.config({
     foxnicUpload: 'upload/foxnic-upload'
 })
 //
-layui.define(['form', 'table', 'util', 'settings', 'admin', 'upload','foxnic','xmSelect','laydate','foxnicUpload'],function () {
+layui.define(['form', 'table', 'util', 'settings', 'admin', 'upload','foxnic','xmSelect','laydate','foxnicUpload','dropdown'],function () {
 
-    var admin = layui.admin,settings = layui.settings,form = layui.form,upload = layui.upload,laydate= layui.laydate;
+    var admin = layui.admin,settings = layui.settings,form = layui.form,upload = layui.upload,laydate= layui.laydate,dropdown=layui.dropdown;
     table = layui.table,layer = layui.layer,util = layui.util,fox = layui.foxnic,xmSelect = layui.xmSelect,foxup=layui.foxnicUpload;
 
     //列表页的扩展
@@ -16,17 +16,50 @@ layui.define(['form', 'table', 'util', 'settings', 'admin', 'upload','foxnic','x
         /**
          * 查询前调用
          * */
-        beforeQuery:function (condition) {
-
+        beforeQuery:function (conditions) {
+            console.log('beforeQuery',conditions);
+        },
+        /**
+         * 表格右侧操作列更多按钮事件
+         * */
+        moreAction:function (menu,data, it){
+            console.log('moreAction',menu,data,it);
+        },
+        testToolButton1:function (selected,it){
+            console.log('testToolButton1',selected,it);
+        },
+        testToolButton2:function (selected,it){
+            console.log('testToolButton2',selected,it);
         },
         doTestAction:function (data){
-            alert("doTest")
+            console.log('doTestAction',data);
+        },
+        other:function(){
+
         }
     }
 
     //表单页的扩展
     var form={
-
+        /**
+         * 表单数据填充前
+         * */
+        beforeDataFill:function (data) {
+            console.log('beforeDataFill',data);
+        },
+        /**
+         * 表单数据填充后
+         * */
+        afterDataFill:function (data) {
+            console.log('afterDataFill',data);
+        },
+        /**
+         * 数据提交前，如果返回 false，停止后续步骤的执行
+         * */
+        beforeSubmit:function (data) {
+            console.log("beforeSubmit",data);
+            return true;
+        }
     }
     //
     window.pageExt={form:form,list:list};
