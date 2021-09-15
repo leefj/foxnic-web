6e8f23ba55f61ad9166c35c9eee6e6ff5e6ae964
lifangjie <leefangjie@qq.com>
2021-09-15 11:05:15 +0800

    删除不需要的代码

--- common/generator/src/main/java/org/github/foxnic/web/generator/module/hrm/HrmEmployeePositionConfig.java
+++ common/generator/src/main/java/org/github/foxnic/web/generator/module/hrm/HrmEmployeePositionConfig.java
@@ -38,11 +38,11 @@ public class HrmEmployeePositionConfig extends BaseCodeConfig<HRM_EMPLOYEE_POSIT
     public void configOverrides() {
         this.context.overrides()
             .setServiceIntfAnfImpl(WriteMode.COVER_EXISTS_FILE) //服务与接口
-            .setControllerAndAgent(WriteMode.COVER_EXISTS_FILE) //Rest
-            .setPageController(WriteMode.COVER_EXISTS_FILE) //页面控制器
-            .setFormPage(WriteMode.COVER_EXISTS_FILE) //表单HTML页
-            .setListPage(WriteMode.COVER_EXISTS_FILE) //列表HTML页
-            .setExtendJsFile(WriteMode.COVER_EXISTS_FILE);
+            .setControllerAndAgent(WriteMode.IGNORE) //Rest
+            .setPageController(WriteMode.IGNORE) //页面控制器
+            .setFormPage(WriteMode.IGNORE) //表单HTML页
+            .setListPage(WriteMode.IGNORE) //列表HTML页
+            .setExtendJsFile(WriteMode.IGNORE);
     }
 
 }
