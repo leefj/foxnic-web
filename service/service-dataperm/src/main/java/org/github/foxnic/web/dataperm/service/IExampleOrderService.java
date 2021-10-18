1be2711c4573c01f40d4b21710137ffe42b79771
lifangjie <leefangjie@qq.com>
2021-10-18 09:18:22 +0800

    代码生成，指定搜索字段

--- service/service-dataperm/src/main/java/org/github/foxnic/web/dataperm/service/IExampleOrderService.java
+++ service/service-dataperm/src/main/java/org/github/foxnic/web/dataperm/service/IExampleOrderService.java
@@ -21,7 +21,7 @@ import com.github.foxnic.dao.data.SaveMode;
  * 销售订单表 服务接口
  * </p>
  * @author 李方捷 , leefangjie@qq.com
- * @since 2021-10-17 07:10:24
+ * @since 2021-10-18 09:17:48
 */
 
 public interface IExampleOrderService extends ISuperService<ExampleOrder> {
