27079d6592ea8f62fafb8ef4aa6a500e917a8b09
lifangjie <leefangjie@qq.com>
2022-08-27 17:37:13 +0800

    狠狠修复了一波bug

--- common/framework-boot/src/main/java/org/github/foxnic/web/framework/starter/BootApplication.java
+++ common/framework-boot/src/main/java/org/github/foxnic/web/framework/starter/BootApplication.java
@@ -3,6 +3,8 @@ package org.github.foxnic.web.framework.starter;
 import com.github.foxnic.commons.code.CodeBuilder;
 import com.github.foxnic.commons.environment.Environment;
 import com.github.foxnic.commons.io.FileUtil;
+import com.github.foxnic.commons.log.Logger;
+import com.github.foxnic.springboot.starter.BootArgs;
 import com.github.foxnic.springboot.starter.FoxnicApplication;
 import org.aspectj.weaver.ast.Test;
 import org.github.foxnic.web.framework.FoxnicWebMeta;
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Enumeration;
 
 
@@ -19,13 +22,41 @@ import java.util.Enumeration;
 public class BootApplication {
 
 	public static ConfigurableApplicationContext run(Class<?> bootType, String... args) {
+
+		Logger.DIRECT=true;
+		long t=System.currentTimeMillis();
+		Logger.info("Foxnic-Web Booting...");
+
+		for (String arg:args) {
+			Logger.info("Original Parameter : "+arg);
+		}
+
+		//初始化参数
+		BootArgs.initOnBoot(args);
+
+		Logger.info("Is In IDE : "+BootArgs.isBootInIDE());
+		Logger.info("Work Dir : "+BootArgs.getWorkDir().getAbsolutePath());
+
+		Logger.DIRECT=false;
+
 		Environment.getEnvironment().init();
 		ConfigurableApplicationContext context=new SpringApplication(new Class[] {FoxnicApplication.class,BootApplication.class,bootType}).run(args);
 		return context;
     }
 
 
-	public static void printJar() throws Exception{
+	private static boolean IS_APPLICATION_READY=false;
+
+	/**
+	 * 当前应用是否已经启动就绪
+	 * */
+	public static boolean isApplicationReady()
+	{
+		return IS_APPLICATION_READY;
+	}
+
+
+	public static void printJars() throws Exception {
 
 		CodeBuilder code=new CodeBuilder();
 		ClassLoader classLoader = Test.class.getClassLoader();
